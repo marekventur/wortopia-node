19c460af94cac2af507b3d995a5a188b0d5cd962
Marek Ventur <marekventur@gmail.com>
2015-06-28 15:50:00 +0100

    Uriencode password recovery token

--- src/RecoverLinkManager.js
+++ src/RecoverLinkManager.js
@@ -3,18 +3,18 @@ module.exports = function(config, messageAuthenticationCodeManager, userDao) {
 
     this.createUrl = function(user) {
         var payload = {u: user.id, t: 'pwr', h: user.getPasswordHash()};
-        return config.rootUrl + '/recover?t=' + messageAuthenticationCodeManager.create(payload, user.getPasswordHash());
+        return config.rootUrl + '/recover?t=' + encodeURIComponent(messageAuthenticationCodeManager.create(payload, user.getPasswordHash()));
     }
 
     this.verifyUser = function(token) {
         var payload = messageAuthenticationCodeManager.verify(token);
         if (payload.t !== 'pwr') {
-            throw new Error('This is not a valid recover token');
+            throw new Error('This is not a valid recover token [1]');
         }
         return userDao.getById(payload.u)
         .then(function(user) {
             if (!user || payload.h !== user.getPasswordHash()) {
-                throw new Error('This is not a valid recover token');
+                throw new Error('This is not a valid recover token [2]');
             }
             return user;
         })
