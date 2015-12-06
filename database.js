64b12087cb90d653de2ed669ea1ea38614fde83f
Marek Ventur <marekventur@gmail.com>
2015-12-06 00:11:42 +0000

    More travis stuff

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
 module.exports = {
     "test": "postgres://wortopia:wortopia@localhost/wortopia_test",
     "dev": "postgres://wortopia:wortopia@localhost/wortopia_dev",
-    "prod": "postgres://wortopia:wortopia@localhost/wortopia",
+    "prod": "postgres://wortopia:wortopia@localhost/wortopia"
+
 }
